c699f16d9bcc0ab8521b63437fd7deac2796ffc3
Timonko <39344326+Tlmonko@users.noreply.github.com>
2020-11-25 12:32:06 +0500

    created new task creating button

--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <h1>Tasks</h1>
+        <h1 class="Title">Tasks</h1>
         <div class="counter">
             Completed tasks: {{ getComplitedTasks }}
         </div>
@@ -8,6 +8,14 @@
             <TodoCard
                     v-for="card of getTodos" :key="card"
                     v-bind:Card="card"/>
+            <a class="new-task">
+                <div class="new-task-div">
+                    <div class="new-task-text">
+                        <h1 class="plus">+</h1>
+                        <p>Create new task</p>
+                    </div>
+                </div>
+            </a>
         </div>
     </div>
 </template>
@@ -28,6 +36,9 @@
 </script>
 
 <style lang="scss" scoped>
+    $card-width: 325px;
+    $card-height: 150px;
+
     .main-container {
         display: flex;
         margin: 100px 30vh;
@@ -36,7 +47,7 @@
         padding: 0;
     }
 
-    h1 {
+    .Title {
         font-size: 48px;
         line-height: 56px;
         color: #323232;
@@ -59,5 +70,58 @@
         line-height: 21px;
         text-align: center;
         color: #323232;
+    }
+
+    .new-task-div {
+        position: relative;
+        display: flex;
+        justify-content: center;
+        width: $card-width;
+        height: $card-height;
+        background-color: #F7F7F7;
+        border-radius: 9px;
+        padding: 5px;
+        margin: 10px;
+        align-content: center;
+    }
+
+    .plus {
+        margin: 0;
+        color: #999999;
+        font-weight: bolder;
+        -webkit-touch-callout: none;
+        -webkit-user-select: none;
+        -khtml-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+    }
+
+    .new-task-text {
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50%);
+    }
+
+    p {
+        font-family: 'Roboto', sans-serif;
+        font-style: normal;
+        font-weight: bolder;
+        font-size: 18px;
+        line-height: 21px;
+        display: flex;
+        align-items: center;
+        text-align: center;
+        color: #323232;
+        -webkit-touch-callout: none;
+        -webkit-user-select: none;
+        -khtml-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+    }
+
+    .new-task-div:hover {
+        background-color: #F0F0F0;
     }
 </style>
