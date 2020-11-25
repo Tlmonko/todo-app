adeb1dc6ce0e704b9fd99f4877283951721c8a86
Timonko <39344326+Tlmonko@users.noreply.github.com>
2020-11-25 14:08:11 +0500

    created new task form closing button

--- src/components/NewTask.vue
+++ src/components/NewTask.vue
@@ -1,10 +1,13 @@
 <template>
     <div v-bind:class="{displayNone: !this.getNowCreatingTask}">
         <form @submit.prevent="submit">
-            <h1>New Task</h1>
+            <div class="header">
+                <h1>New Task</h1>
+                <span class="close" v-on:click="close"></span>
+            </div>
             <h2>Task name</h2>
             <input placeholder="Enter task name" type="text" v-model="taskName"/><br>
-            <button type="submit">
+            <button class="my-btn" type="submit">
                 Create
             </button>
         </form>
@@ -22,12 +25,46 @@
 			submit() {
 				this.createTodo(this.taskName)
 				this.changeState()
-			}
+			},
+            close() {
+				this.changeState()
+            }
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
+    .close{
+        position: absolute;
+        right: 0;
+        width: 55px;
+        height: 55px;
+        cursor: pointer;
+        border: 1px solid #999999;
+        border-radius: 50%;
+    }
+
+    .close:before,
+    .close:after {
+        content: "";
+        position: absolute;
+        top: 27px;
+        left: 15px;
+        width: 26px;
+        height: 1px;
+        background: #999999;
+    }
+
+    .close:before {
+        webkit-transform: rotate(45deg);
+        transform: rotate(45deg);
+    }
+
+    .close:after {
+        webkit-transform: rotate(-45deg);
+        transform: rotate(-45deg);
+    }
+
     .displayNone {
         display: none !important;
     }
@@ -46,7 +83,19 @@
     $padding-left-right-btn: 130px;
     $padding-top-bottom-btn: 16px;
 
-    button {
+    h1 {
+        margin: 0 150px;
+    }
+
+    .close-btn {
+        width: 81px;
+        height: 81px;
+        border-radius: 97px;
+        border: 1px solid #999999;
+        background-color: #FFFFFF00;
+    }
+
+    .my-btn {
         color: #323232;
         background-color: $color-btn;
         border-radius: 10px;
@@ -61,11 +110,11 @@
         margin: 100px 20px 20px;
     }
 
-    button:hover {
+    .my-btn:hover {
         box-shadow: 0 2px 15px rgba(0, 0, 0, 0.1);
     }
 
-    button:focus {
+    button {
         outline: none;
     }
 
